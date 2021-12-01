4d0b21d70d9be50f5936d307bd37c226246a28fe
Troy Pena <troy.pena@MCUSA.local>
2021-12-01 11:32:33 -0500

    k

--- test.py
+++ test.py
@@ -1,13 +1,5 @@
-import time
 
-SLEEP_TIME = 2
+SHOULD_I = True
 
-print("STARTING NOW!")
-
-input1 = input("Hello, place an input1: \n")
-print("Your input1 value is", input1)
-
-input2 = input("Great, now place input2: \n")
-print("Your input2 value is", input2)
-
-time.sleep(SLEEP_TIME)
+if SHOULD_I:
+    print("Hello!")
