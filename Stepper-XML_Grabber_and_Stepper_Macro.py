1afe2a6266690c4d1cecfa39df4087a56a30096d
Troy Pena <troy.pena@MCUSA.local>
2021-12-01 11:21:40 -0500

    Works now

--- Stepper-XML_Grabber_and_Stepper_Macro.py
+++ Stepper-XML_Grabber_and_Stepper_Macro.py
@@ -8,13 +8,13 @@ import os
 import pandas as pd
 import time
 import math
-import sys
-from subprocess import Popen, PIPE, STDOUT
+from pynput.keyboard import Key, Controller
 
 
 # User Parameters/Constants to Set
 XML_DIR = "./XML_Files/"
-NUM_MAP_SITES = 6
+SLEEP_TIME_BETWEEN = 0.05
+SLEEP_TIME_END = 5 
 
 
 def time_convert(sec):
@@ -23,6 +23,23 @@ def time_convert(sec):
   hours = mins // 60
   mins = mins % 60
   print("Time Lapsed = {0}h:{1}m:{2}s".format(int(hours) ,int(mins), round(sec)))
+
+
+# Presses Alt + Tab
+def altTab():
+    time.sleep(SLEEP_TIME_BETWEEN)
+    with keyboard.pressed(Key.alt):
+        keyboard.press(Key.tab)
+        keyboard.release(Key.tab)
+
+
+# Type's in string
+def typeThis(toType):
+    time.sleep(SLEEP_TIME_BETWEEN)
+    keyboard.type(toType)
+    time.sleep(SLEEP_TIME_BETWEEN)
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
 
 
 
@@ -34,6 +51,8 @@ start_time = time.time()
 
 # Clears some of the screen for asthetics
 print("\n\n\n\n\n\n\n\n\n\n\n\n\n")
+
+keyboard = Controller()
 
 # Runs through each XML file in folder placement
 for XML_Name in os.listdir(XML_DIR):
@@ -72,74 +91,74 @@ for XML_Name in os.listdir(XML_DIR):
     rear_blade  = df['Value'][21]
     front_blade = df['Value'][22]
     
-    print('UPDATE CREATION DATE: Y')
-    print('JOB COMMENT: ' + 'job name')
-    print('TOLERANCE: 3')
-    print('SCALE CORRECTIONS \n X, PPM: 0 \n Y, PPM: 0 \n ORTHOGONALITY, PPM:0 \n LEVELER BATCH SIZE:1')
+    #print('UPDATE CREATION DATE: Y')
+    #print('JOB COMMENT: ' + 'job name')
+    #print('TOLERANCE: 3')
+    #print('SCALE CORRECTIONS \n X, PPM: 0 \n Y, PPM: 0 \n ORTHOGONALITY, PPM:0 \n LEVELER BATCH SIZE:1')
     
     
-    if wafer_size == 200:
-        print('Wafer diameter: 215')
-    elif wafer_size == 150:
-        print('Wafer diameter: 180')
-    elif wafer_size == 100:
-        print('\n', 'Wafer diameter: 160')
-    else:
-        print('Unsupported wafer size')
+    # if wafer_size == 200:
+    #     #print('Wafer diameter: 215')
+    # elif wafer_size == 150:
+    #     #print('Wafer diameter: 180')
+    # elif wafer_size == 100:
+    #     #print('\n', 'Wafer diameter: 160')
+    # else:
+    #     #print('Unsupported wafer size')
     
-    print('\n', 'STEP SIZE:', '\n', 'X:', stepdist_x / 1000, '\n COUNT', '\n HOW MANY COLUMNS?', step_x, '\n', 'STEP SIZE \n Y:', 
-          stepdist_y / 1000, '\n COUNT', '\n HOW MANY ROWS?', step_y, '\n')
+    #print('\n', 'STEP SIZE:', '\n', 'X:', stepdist_x / 1000, '\n COUNT', '\n HOW MANY COLUMNS?', step_x, '\n', 'STEP SIZE \n Y:', 
+          # stepdist_y / 1000, '\n COUNT', '\n HOW MANY ROWS?', step_y, '\n')
     
-    if wafer_size == 100:
-        print('TRANSLATE ORIGIN:', '\n', 'X: -18', '\n', 'Y: 20', '\n')
-    else:
-        print('TRANSLATE ORIGIN:', '\n', 'X: 0', '\n', 'Y: 0', '\n')
+    # if wafer_size == 100:
+    #     #print('TRANSLATE ORIGIN:', '\n', 'X: -18', '\n', 'Y: 20', '\n')
+    # else:
+    #     #print('TRANSLATE ORIGIN:', '\n', 'X: 0', '\n', 'Y: 0', '\n')
         
-    print('DISPLAY?: N \nLAYOUT?: N \nADJUST: N')
+    #print('DISPLAY?: N \nLAYOUT?: N \nADJUST: N')
 
     rkey_xoffset = round(((-1*(step_x/2-0.5)*stepdist_x+(step_x-rkey_C)*stepdist_x) - rkey_x) / 1000, 5)
     rkey_yoffset = round(((rkey_y - (-1*(step_y/2-0.5)*stepdist_y+(rkey_R-1)*stepdist_y) ) / 1000), 5)
     lkey_xoffset = round((((-1*(step_x/2-0.5)*stepdist_x+(step_x-lkey_C)*stepdist_x) - lkey_x) / 1000), 5)
     lkey_yoffset = round(((lkey_y - (-1*(step_y/2-0.5)*stepdist_y+(lkey_R-1)*stepdist_y)) / 1000), 5)
     
-    print ('ALIGNMENT PARAMATERS \nSTANDARD KEYS?: N\nRIGHT ALIGNMENT DIE CENTER:', '\n', 'R:', int(rkey_R), '\n', 'C:', int(rkey_C)
-           , '\nRIGHT KEY OFFSET\nX:', rkey_xoffset, '\nY:', rkey_yoffset, '\nLEFT ALGINMENT DIE CENTER:', '\n', 'R:', int(lkey_R), '\n', 
-           'C:', int(lkey_C), '\nLEFT KEY OFFSET\nX:', lkey_xoffset, '\nY:', lkey_yoffset, '\n')
+    #print ('ALIGNMENT PARAMATERS \nSTANDARD KEYS?: N\nRIGHT ALIGNMENT DIE CENTER:', '\n', 'R:', int(rkey_R), '\n', 'C:', int(rkey_C)
+           # , '\nRIGHT KEY OFFSET\nX:', rkey_xoffset, '\nY:', rkey_yoffset, '\nLEFT ALGINMENT DIE CENTER:', '\n', 'R:', int(lkey_R), '\n', 
+           # 'C:', int(lkey_C), '\nLEFT KEY OFFSET\nX:', lkey_xoffset, '\nY:', lkey_yoffset, '\n')
     
-    print('EPI SHIFT\nX:\nY:')
+    #print('EPI SHIFT\nX:\nY:')
     
 
-    print('\n\nPASS')
+    #print('\n\nPASS')
     #FIX#
-    print('\nNAME:', 'name of pass')
-    print('\nPASS COMMENT:\n', 'name of pass comment')
-    print('\nUSE LOCAL ALIGNMENT?: N')
-    print('ENABLE MATCH ?: N')
+    #print('\nNAME:', 'name of pass')
+    #print('\nPASS COMMENT:\n', 'name of pass comment')
+    #print('\nUSE LOCAL ALIGNMENT?: N')
+    #print('ENABLE MATCH ?: N')
     
     pshift_x = round((((-1*(step_x/2-0.5)*stepdist_x) + mask_x) - wafer_x) / 1000, 8)
     pshift_y = round((wafer_y - ((-1*(step_y/2-0.5)*stepdist_y) + mask_y) ) / 1000, 8)
-    print('\nPASS SHIFT:\nX:', pshift_x, '\nY:', pshift_y, '\n')
+    #print('\nPASS SHIFT:\nX:', pshift_x, '\nY:', pshift_y, '\n')
 
     
     left_b = (left_blade * 5) / 1000 + 50
     right_b = 50 - (right_blade * 5) / 1000
     front_b = (front_blade * 5) / 1000 + 50
     rear_b = 50 - (rear_blade * 5) / 1000
-    print('MASKING APERTURE SETTINGS:\nXL:', left_b, '\nXR:', right_b, '\nYF:', front_b, '\nYR:', rear_b)
-    print('RETICLE ALIGNMENT OFFSET (MICRONS) :\nXL:0\nXR:0\nY:0')
-    print('RETICLE ALIGNMENT MARK PHASE: N')
-    print('A-RRAY OR P-LUG:', 'array')
+    #print('MASKING APERTURE SETTINGS:\nXL:', left_b, '\nXR:', right_b, '\nYF:', front_b, '\nYR:', rear_b)
+    #print('RETICLE ALIGNMENT OFFSET (MICRONS) :\nXL:0\nXR:0\nY:0')
+    #print('RETICLE ALIGNMENT MARK PHASE: N')
+    #print('A-RRAY OR P-LUG:', 'array')
     
-    print('DROPOUTS:', 'see dropouts')
+    #print('DROPOUTS:', 'see dropouts')
     
-    print('SAVE PASS? : Y')
+    #print('SAVE PASS? : Y')
     
     i = 0
     while True:
         df_p_i = pd.read_excel(XML_Path, sheet_name = i+2) 
         if math.isnan(df_p_i['Number of Plugs'][0]):
             break
-        print('\n', 'Pass Name:', df_p_i['Pass Name'][0], '\n')
+        #print('\n', 'Pass Name:', df_p_i['Pass Name'][0], '\n')
         test_mask_x = df_p_i['Test site bottom left x on mask'][0]
         test_mask_y = df_p_i['Test site bottom left y on mask'][0]
         left_blade  = df_p_i['Plug Pass Reticle Blade Left Position'][0]
@@ -153,8 +172,13 @@ for XML_Name in os.listdir(XML_DIR):
         front_b = (front_blade * 5) / 1000 + 50
         rear_b  = 50 - (rear_blade * 5) / 1000
     
-        print('Aperture Blades:', '\n', 'Left:', left_b, '\n', 'Right:', right_b, '\n', 'Front:', front_b, '\n', 'Rear:', rear_b,)
+        #print('Aperture Blades:', '\n', 'Left:', left_b, '\n', 'Right:', right_b, '\n', 'Front:', front_b, '\n', 'Rear:', rear_b,)
         num_plugs = int(df_p_i['Number of Plugs'][0])
+        
+        # REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+        test = input("Press enter to continue autotyping first set.")
+        altTab()
+        # REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
         
         for x in range(num_plugs):
             x_offset = round((((-1*(step_x/2-0.5)*stepdist_x+(step_x-df_p_i['Plug Closest Column'][x])*stepdist_x) + test_mask_x) - 
@@ -162,23 +186,42 @@ for XML_Name in os.listdir(XML_DIR):
             y_offset = round((df_p_i['Bottom Left y on wafer'][x] - ((-1*(step_y/2-0.5)*stepdist_y+
                                                                (df_p_i['Plug Closest Row '][x]-1)*stepdist_y)
                                                               + test_mask_y)) / 1000, 5)
-            print('\n', 'Plug', str(df_p_i['Plug Number '][x]) + ':', '\n', 'R:', df_p_i['Plug Closest Row '][x], '\t', 'y:', 
-              y_offset, '\n',
-              'C:', df_p_i['Plug Closest Column'][x], '\t', 'x:', x_offset)
+            # print('\n', 'Plug', str(df_p_i['Plug Number '][x]) + ':', '\n', 'R:', df_p_i['Plug Closest Row '][x], '\t', 'y:', 
+            #   y_offset, '\n',
+            #   'C:', df_p_i['Plug Closest Column'][x], '\t', 'x:', x_offset)
+            
+            # REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+            typeThis(str(df_p_i['Plug Number '][x]))
+            typeThis(str(df_p_i['Plug Closest Row '][x]))
+            typeThis(str(y_offset))
+            typeThis(str(df_p_i['Plug Closest Column'][x]))
+            typeThis(str(x_offset))
+            # REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+            
+        # REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+        altTab()
+        # REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
         
         i += 1
-            
-            
-    print('PASS\nNAME: MAP\nPASS COMMENT:\nMAPPING')
-    print('USE LOCAL ALIGNMENT?: Y')
-    print('EXPOSE MAPPING PASS?: N\nUSE TWO POINT ALIGNMENT?:\nNUMBER OF ALIGNMENTS PER DIE?:1')
-    print('LOCAL ALIGNMENT MARK OFFSET:\nX:0\nY:0\nMONITOR MAPPING CORRECTIONS?:Y')
-    print('MAP EVERY N TH WAFER N = 1\nMICROSCOPE FOCUS OFFSET:0\nPASS SHIFT:\nX:0\nY:0\nA-RRAY OR P-LUG: P')
 
-    print('\n\nPLUGS:\n')
+
+            
+    #print('PASS\nNAME: MAP\nPASS COMMENT:\nMAPPING')
+    #print('USE LOCAL ALIGNMENT?: Y')
+    #print('EXPOSE MAPPING PASS?: N\nUSE TWO POINT ALIGNMENT?:\nNUMBER OF ALIGNMENTS PER DIE?:1')
+    #print('LOCAL ALIGNMENT MARK OFFSET:\nX:0\nY:0\nMONITOR MAPPING CORRECTIONS?:Y')
+    #print('MAP EVERY N TH WAFER N = 1\nMICROSCOPE FOCUS OFFSET:0\nPASS SHIFT:\nX:0\nY:0\nA-RRAY OR P-LUG: P')
+
+    #print('\n\nPLUGS:\n')
     
     df_m = pd.read_excel(XML_Path, sheet_name = 'Mapping')
     i = 0
+    
+    # REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+    test = input("Press enter to continue auto typing second set.")
+    altTab()
+    # REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+    
     while True:
         if math.isnan(df_m['Closest Column'][i]):
             break
@@ -187,21 +230,43 @@ for XML_Name in os.listdir(XML_DIR):
         y_offset = round(((df_m['Center of alignment site y on wafer'][i] -
                            (-1*(step_y/2-0.5)*stepdist_y+(df_m['Closest Row'][i]-1)*stepdist_y) 
                            )) / 1000, 5)
-        print('Plug', str(df_m['Map Site Number'][i]) + ':', '\n', 'R:', df_m['Closest Row'][i], '\t', 'y:', y_offset, '\n', 
-             'C:', df_m['Closest Column'][i], '\t', 'x:', x_offset)
+        #print('Plug', str(df_m['Map Site Number'][i]) + ':', '\n', 'R:', df_m['Closest Row'][i], '\t', 'y:', y_offset, '\n', 
+             # 'C:', df_m['Closest Column'][i], '\t', 'x:', x_offset)
+        
+        # REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+        
+        typeThis(str(df_m['Map Site Number'][i]))
+        typeThis(str(df_m['Closest Row'][i]))
+        typeThis(str(y_offset))
+        typeThis(str(df_m['Closest Column'][i]))
+        typeThis(str(x_offset))
+        # REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+        
         i += 1
         
         
-    print('NAME (<CR> TO EXIT PASS SETUP) :')
-    print('WRITE TO DISK?:Y')
-    print('PURGE EDITED FILES ? : Y')
+      
+    # REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+    altTab()
+    # REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+    
+    #print('NAME (<CR> TO EXIT PASS SETUP) :')
+    #print('WRITE TO DISK?:Y')
+    #print('PURGE EDITED FILES ? : Y')
     # -------------------------------------------------------------------------
     
     
-    # Macro
-    # -------------------------------------------------------------------------
+# Macro
+# -----------------------------------------------------------------------------
 
 
+
+
+
+
+        
+
+# -----------------------------------------------------------------------------
 
 print("\nThis program is done!")
 
--- test4.py
+++ test4.py
@@ -1,41 +1,57 @@
 import time
-import os
-import sys
-from subprocess import Popen, PIPE, STDOUT
+from pynput.keyboard import Key, Controller
 
 
 # User parameters to set: 
-SLEEP_TIME_BETWEEN = 1 
-SLEEP_TIME_END = 3 
+SLEEP_TIME_BETWEEN = 0.5 
+SLEEP_TIME_END = 5 
+
+
+def time_convert(sec):
+  mins = sec // 60
+  sec = sec % 60
+  hours = mins // 60
+  mins = mins % 60
+  print("Time Lapsed = {0}h:{1}m:{2}s".format(int(hours) ,int(mins), round(sec)))
+
+
+# Presses Alt + Tab
+def altTab():
+    time.sleep(SLEEP_TIME_BETWEEN)
+    with keyboard.pressed(Key.alt):
+        SLEEP_TIME_BETWEEN
+        keyboard.press(Key.tab)
+        keyboard.release(Key.tab)
+
+
+# Type's in string
+def typeThis(toType):
+    time.sleep(0.25)
+    keyboard.type(toType)
+    time.sleep(0.25)
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
 
 
 
-script_path = 'test.py'
+# Starting stopwatch to see how long process takes
+start_time = time.time()
 
-p = Popen([sys.executable, script_path], 
-          stdout=PIPE, 
-          stdin=PIPE,
-          )
+# Clears some of the screen for asthetics
+print("\n\n\n\n\n\n\n")
+
+keyboard = Controller()
 
 while True:
-    stdout = p.stdout.readline()
-    print(stdout)
+    test = input("Press enter to continue.")
     
-    if "place" in str(stdout):
-        p.stdin.write("OKAY".encode())
-        p.stdin.flush()
-        p.stdout.close()
-        # print(p.stdout.readline())
-
-# with p.stdout:
-#     for line in iter(p.stdout.readline, b''):
-#         line = str(line).replace("b'", "").replace("\\r\\n","").replace("'", "")
-#         print(line)
-#         if "place" in line:
-#             time.sleep(SLEEP_TIME_BETWEEN)
-#             p.stdin.write("OKAY".encode())
-#             p.stdin.close()
-# p.wait()
+    altTab()
+    
+    typeThis("Hello World")
 
 
-time.sleep(SLEEP_TIME_END)
+# Starting stopwatch to see how long process takes
+print("\n\nTotal Time: ")
+end_time = time.time()
+time_lapsed = end_time - start_time
+time_convert(time_lapsed)
